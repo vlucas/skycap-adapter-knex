bc2830b92dcda0be73c0db25ed04f12bdebb2bb0
Vance Lucas <vance@vancelucas.com>
2019-01-05 00:58:49 -0600

    Working user registration

--- migrations/20170224095524_create_users_table.js
+++ migrations/20170224095524_create_users_table.js
@@ -1,18 +1,17 @@
-'use strict';
-
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
   return knex.schema.createTable('users', function(t) {
     t.increments('id').primary();
     t.string('name').notNull();
+    t.string('username').nullable().defaultTo(null).unique();
     t.string('email').notNull().unique();
     t.string('password').notNull();
     t.boolean('is_admin').defaultTo(false);
-    t.json('profile').nullable();
+    t.json('profileData').nullable();
     t.dateTime('dt_created').notNull();
     t.dateTime('dt_updated').nullable();
   });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex) {
   return knex.schema.dropTable('users');
 };
--- src/users.js
+++ src/users.js
@@ -18,22 +18,31 @@ function findByEmail(email) {
 /**
  * Register a new user
  *
+ * @param name {String}
  * @param email {String}
  * @param hashedPassword {String}
+ * @param username {String} - Optional username
  * @param profileData {Object}
  *
  * @return {Promise}
  */
-function register(email, hashedPassword, profileData = {}) {
+function register(name, email, hashedPassword, username = null, profileData = {}) {
   return findByEmail(email)
     .then(user => {
       // Ensure user does not already exist
-      if (user !== false) {
+      if (user) {
         return Promise.reject('User already exists!');
       }
 
       let NOW = new Date();
+
+      if (Object.keys(profileData).length === 0) {
+        profileData = null;
+      }
+
       let storedUser = {
+        name,
+        username,
         email,
         password: hashedPassword,
         profileData,
@@ -42,7 +51,10 @@ function register(email, hashedPassword, profileData = {}) {
       };
 
       return getConnection()(TABLE_USERS)
-        .insert(storedUser);
+        .insert(storedUser)
+        .then(() => {
+          return findByEmail(email);
+        });
     });
 }
 
